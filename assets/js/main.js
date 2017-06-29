edf4ae90479d32fe19698c4b015ee9e4ac3a5a72
franshu <fran.ojeda208@gmail.com>
2017-06-29 12:02:24 -0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+
+
+$(document).ready(function(){
+  $('.add-items').submit(function(event) 
+  {
+    event.preventDefault();
+    var item= $("#item").val();
+    list-items
+   if(e.which == 13) {// cuando presiona enter ingresar imput
+    else {
+      $("#todo").append('<div><span class="center-align">'+ item +'</p></div>') // ingresar item en tofo
+}
+ 
+});
+
+});
